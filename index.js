49353bce6cb80d0e13734880cea79408bf8f6f2b
+Michael Kolb <+michael.w.kolb@gmail.com>
2021-12-03 10:04:57 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution here
+
+function findMatching(driverArray, nameSearched) {
+    let driverMatch = driverArray.filter(function (element) {
+        return element.toLowerCase() === nameSearched.toLowerCase();  
+    })
+    return driverMatch;
+}
+
+function fuzzyMatch(driverArray, partialSearch) {
+    let driverMatch = driverArray.filter(function (element) {
+        if (partialSearch === partialSearch.toLowerCase()) {
+            return;
+        }
+        else {
+            return element.includes(partialSearch);
+        }
+    })
+    return driverMatch;
+    
+}
+
+function matchName(driverArray, nameSearched) {
+    let driverMatch = driverArray.filter(function(element) {
+        return element.name === nameSearched;
+    })
+    return driverMatch;
+}
